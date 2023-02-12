6d1890bc46a3924b137dcb84831b92ef06a67f19
Khang Le <khangle@Khangs-Mac-mini.local>
2023-02-12 22:43:08 +0700

    add test assigment

--- test_Assignment.py
+++ test_Assignment.py
@@ -2,10 +2,34 @@ import unittest
 from unittest import TestCase
 
 
-class TestAssigment(TestCase):
+class TestAssignment(TestCase):
 
-    def test_same_id(self):
-        self.assertEqual(1, 1)
+    def test_assignment_int(self):
+        a = 1
+        b = a
+        self.assertEqual(id(a), id(b))
+        b = 2
+        self.assertNotEqual(id(a), id(b))
+
+    def test_assignment_string(self):
+        a = 'apple'
+        b = a
+        self.assertEqual(id(a), id(b))
+        b = 'pear'
+        self.assertNotEqual(id(a), id(b))
+
+    def test_assignment_object(self):
+        a = Obj()
+        b = a
+        self.assertEqual(id(a), id(b))
+        b.x = 2
+        self.assertEqual(a.x, b.x)
+        self.assertEqual(id(a), id(b))
+
+
+class Obj:
+    def __init__(self):
+        self.x = 1
 
 
 if __name__ == '__main__':
