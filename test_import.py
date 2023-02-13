86eb561db386e887912685b4b846be2e00fcd6c4
khangHNISC <59554204+khangHNISC@users.noreply.github.com>
2023-02-13 14:46:59 +0700

    add import

---
+++ test_import.py
@@ -0,0 +1,11 @@
+import fibo
+from fibo import fib, fib2
+from fibo import *
+import fibo as fib
+from fibo import fib as abc
+import sys
+
+if __name__ == '__main__':
+    print(dir(sys))
+    print(dir())
+    print(sys.path)
